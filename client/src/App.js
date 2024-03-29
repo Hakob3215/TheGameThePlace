e3e8996084b2ac6db63e6766969a691c26b48c28
Hakob3215 <hatajyan@gmail.com>
2024-03-29 15:47:08 -0700

    Add FrontPage and basic server setup (port listen)

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import FrontPage from './pages/FrontPage';
+
 
 const App = () => {
     return (
         <Router>
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/about" component={AboutPage} />
-                <Route path="/contact" component={ContactPage} />
-            </Switch>
+            <Routes>
+                <Route path="/" element={<FrontPage/>} />
+            </Routes>
         </Router>
     );
 }
--- client/src/pages/frontPage.js
+++ client/src/pages/frontPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import './FrontPage.css';
 
-const frontPage = () => {
+
+const FrontPage = () => {
     return <h1>Front Page!</h1>;
 }
 
-export default frontPage;
+export default FrontPage;
