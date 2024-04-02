d5d41d2866a3922260398b773c4e1260e8b59acc
Hakob3215 <hatajyan@gmail.com>
2024-04-02 00:22:02 -0700

    Update front desc with cooldown+zoom

--- client/src/pages/FrontPage.js
+++ client/src/pages/FrontPage.js
@@ -15,7 +15,7 @@ const FrontPage = () => {
             </div>
             }
             <h1 className='descriptionTitle'>What even is this?</h1>
-            <p className='description'>[<strong>It is essentially Reddit's r/Place</strong>]<br/> But, its for The Game{'\u2122'} Discord! <br/> <br/> If you don't know what r/Place is... <br/> <br/> &nbsp;&nbsp;&nbsp;&nbsp; It is essentially a canvas comprised of a certain amount of pixels, where each player can pick a color and set one pixel to that color! Afterwards, they are placed on a cooldown and cannot change the color of another pixel until the cooldown is over.</p>
+            <p className='description'>[<strong>It is essentially Reddit's r/Place</strong>]<br/> But, its for The Game{'\u2122'} Discord! <br/> <br/> If you don't know what r/Place is... <br/> <br/> &nbsp;&nbsp;&nbsp;&nbsp; It is essentially a canvas comprised of a certain amount of pixels, where each player can pick a color and set one pixel to that color! Afterwards, they are placed on a five minute cooldown and cannot change the color of another pixel until the cooldown is over. [Note: It is recommened to zoom out your browser (Ctrl -) until you can see they light grey side borders to make sure that you are seeing the entire board.] </p>
         <p className='credits'>Created by Hakob Atajyan, 2024</p>
         </>);
 }
