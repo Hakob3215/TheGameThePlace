b8953468804d3de66b3da5d85870caa8f247ac1a
Hakob3215 <hatajyan@gmail.com>
2024-04-01 21:39:33 -0700

    Add sign borders + fix typo

--- client/src/pages/FrontPage.js
+++ client/src/pages/FrontPage.js
@@ -15,7 +15,7 @@ const FrontPage = () => {
             </div>
             }
             <h1 className='descriptionTitle'>What even is this?</h1>
-            <p className='description'>[<strong>It is essentially Reddit's r/Place</strong>]<br/> But, its for The Game{'\u2122'} Discord! <br/> <br/> If you don't know what r/Place is... <br/> <br/> &nbsp;&nbsp;&nbsp;&nbsp; It is essentially a canvase comprised of a certain amount of pixels, where each player can pick a color and set one pixel to that color! Afterwards, they are placed on a cooldown and cannot change the color of another pixel until the cooldown is over.</p>
+            <p className='description'>[<strong>It is essentially Reddit's r/Place</strong>]<br/> But, its for The Game{'\u2122'} Discord! <br/> <br/> If you don't know what r/Place is... <br/> <br/> &nbsp;&nbsp;&nbsp;&nbsp; It is essentially a canvas comprised of a certain amount of pixels, where each player can pick a color and set one pixel to that color! Afterwards, they are placed on a cooldown and cannot change the color of another pixel until the cooldown is over.</p>
         <p className='credits'>Created by Hakob Atajyan, 2024</p>
         </>);
 }
