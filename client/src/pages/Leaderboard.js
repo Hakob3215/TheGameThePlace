d40a69b1bc684514d0cada5d8ef0d0920c9e687f
Hakob3215 <hatajyan@gmail.com>
2024-04-05 18:50:09 -0700

    Update leaderboard styling and add style to top 3

--- client/src/pages/Leaderboard.js
+++ client/src/pages/Leaderboard.js
@@ -25,9 +25,29 @@ const Leaderboard = () => {
             <h1 className='leaderboardTitle'>Pixels Placed</h1>
             <ol className='leaderboardList'>
                 {leaderboard.map((user, index) => {
+                    // first place is gold, second is silver, third is bronze
+                    let isTop3 = true;
+                    let styleColor = '';
+                    let fontSize = '20px';
+                    if (index === 0) {
+                        styleColor = 'gold';
+                        fontSize = '29px';
+                    } else if (index === 1) {
+                        styleColor = 'silver';
+                        fontSize = '26px';
+                    } else if (index === 2) {
+                        styleColor = '#CD7F32';
+                        fontSize = '21px';
+                    } else {
+                        isTop3 = false;
+                    }
                     return (
+                        !isTop3 ?
                         <li key={index} className='leaderboardItem'>
-                            <strong>{user.username}</strong> {user.pixelCount}
+                            <strong>{user.username}</strong> <span style={{fontSize: '30px'}}>{user.pixelCount}</span>
+                        </li> :
+                        <li key={index} className='topLeaderboardItem' style={{backgroundColor: styleColor, boxShadow: `0 0 10px ${styleColor}`}}>
+                            <strong style={{fontSize: fontSize}}>{user.username} </strong> <span style={{fontSize: '30px'}}>{user.pixelCount}</span>
                         </li>
                     )
                 })}
